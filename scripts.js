98e3e362baa2d7fde28c9685052e3bf139427605
MORALES01 <mayralindao_21@hotmail.com>
2016-03-21 18:03:31 -0500

    corregido scripts

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
-onsole.log("Inicio de programa");
-var nombre = promt("Cual es tu nombre?");
-document.write("<h1> Su nombre es  + nombre + ".</p>")
+console.log("Inicio de programa");
+var nombre = prompt("Cual es tu nombre?");
+document.write("<h1> Su nombre es  + nombre + </h1>");
 console.log("Fin de programa");
